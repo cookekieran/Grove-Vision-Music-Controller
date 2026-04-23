29a37a054bb14e43ba2b9131f707f7a1b41f1325
Kieran Cooke <cookekieran3@gmail.com>
2026-04-23 11:31:03 +0100

    added TinyCNN

--- export.py
+++ export.py
@@ -1,65 +1,52 @@
 import torch
 import torch.onnx
+import torch.nn as nn
 
 device = 'cpu'
 
-# 1. THE BLUEPRINT: Copy your CNN class definition from your notebook here
-class CNN(torch.nn.Module):
+class TinyCNN(torch.nn.Module):
     def __init__(self, num_outputs):
-        super(CNN, self).__init__()
-        self.convl = torch.nn.Conv2d(1, 96, kernel_size=11, stride=4, padding=1)
-        self.rll = torch.nn.ReLU()
-        self.maxl = torch.nn.MaxPool2d(kernel_size=3, stride=2)
+        super(TinyCNN, self).__init__()
         
-        self.conv2 = torch.nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2)
-        self.rl2 = torch.nn.ReLU()
-        self.max2 = torch.nn.MaxPool2d(kernel_size=3, stride=2)
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=2, padding=1)
+        self.rl1 = nn.ReLU()
         
-        self.conv3 = torch.nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1)
-        self.rl3 = torch.nn.ReLU()
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1)
+        self.rl2 = nn.ReLU()
+
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1)
+        self.rl3 = nn.ReLU()
+
+        self.pool = nn.AdaptiveAvgPool2d(1)
+        self.flatten = nn.Flatten()
         
-        self.conv4 = torch.nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1)
-        self.rl4 = torch.nn.ReLU()
-        
-        self.conv5 = torch.nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1)
-        self.rl5 = torch.nn.ReLU()
-        self.max3 = torch.nn.MaxPool2d(kernel_size=3, stride=2)
-        
-        self.flatten = torch.nn.Flatten()
-        self.linearl = torch.nn.Linear(256, 4096)
-        self.rl6 = torch.nn.ReLU()
-        self.dropoutl = torch.nn.Dropout(0.5)
-        
-        self.linear2 = torch.nn.Linear(4096, 4096)
-        self.rl7 = torch.nn.ReLU()
-        self.dropout2 = torch.nn.Dropout(0.5)
-        
-        self.linear3 = torch.nn.Linear(4096, num_outputs)
+        self.linear1 = nn.Linear(64, 32)
+        self.rl4 = nn.ReLU()
+        self.dropout1 = nn.Dropout(0.2) # Lower dropout is often better for TinyML
+        self.linear2 = nn.Linear(32, num_outputs)
 
     def forward(self, x):
-        out = self.maxl(self.rll(self.convl(x)))
-        out = self.max2(self.rl2(self.conv2(out)))
-        out = self.rl3(self.conv3(out))
-        out = self.rl4(self.conv4(out))
-        out = self.rl5(self.conv5(out))
-        out = self.max3(out)
-        out = self.flatten(out)
-        out = self.dropoutl(self.rl6(self.linearl(out)))
-        out = self.dropout2(self.rl7(self.linear2(out)))
-        out = self.linear3(out)
-        return out
+        x = self.rl1(self.conv1(x))
+        x = self.rl2(self.conv2(x))
+        x = self.rl3(self.conv3(x))
+        x = self.pool(x)
+        x = self.flatten(x)
+        x = self.rl4(self.linear1(x))
+        x = self.dropout1(x)
+        x = self.linear2(x)
+        return x
 
 def init_weights(m):
     if type(m) == torch.nn.Linear or type(m) == torch.nn.Conv2d:
         torch.nn.init.xavier_uniform_(m.weight)
 
 num_classes = 4
-model = CNN(num_outputs=num_classes).to(device)
+model = TinyCNN(num_outputs=num_classes).to(device)
 model.apply(init_weights)
 
 def export_to_onnx(model_path, output_name="model.onnx"):
     num_classes = 4 
-    model = CNN(num_outputs=num_classes)
+    model = TinyCNN(num_outputs=num_classes)
 
     model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
     
